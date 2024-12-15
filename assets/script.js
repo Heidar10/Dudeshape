b613969fab0c34b57d6f7c44ab53523974e7d596
Heidar10 <geydar.barselona@gmail.com>
2024-12-15 02:04:13 +0200

    /

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const dropdownMenu = document.querySelector(".nav-flex");
+
+menuToggle.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("active");
+});
+
+///////////////
+
+function initReviewSlider() {
+  const slider = document.querySelector(".slider-reviews");
+  const slides = document.querySelectorAll(".reviewer");
+  const prevButtons = document.querySelectorAll(
+    '.arrow-link img[src*="arrow-left"]'
+  );
+  const nextButtons = document.querySelectorAll(
+    '.arrow-link img[src*="arrow-right"]'
+  );
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.display = i === index ? "flex" : "none";
+    });
+  }
+
+  showSlide(currentIndex);
+
+  prevButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      showSlide(currentIndex);
+    });
+  });
+
+  nextButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showSlide(currentIndex);
+    });
+  });
+}
+
+function initCosmeticSlider() {
+  const slider = document.querySelector(".slider-cosmetics");
+  const prevBtn = document.querySelector(".btn-prev");
+  const nextBtn = document.querySelector(".btn-next");
+
+  const images = [
+    "./assets/img/cosmetic-products/black-bottles.png",
+    "./assets/img/cosmetic-products/pink-bottles.png",
+  ];
+
+  let currentIndex = 0;
+
+  const changeSlide = (index) => {
+    slider.style.backgroundImage = `url(${images[index]})`;
+  };
+
+  nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeSlide(currentIndex);
+  });
+
+  prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    changeSlide(currentIndex);
+  });
+
+  changeSlide(currentIndex);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initReviewSlider();
+  initCosmeticSlider();
+});
